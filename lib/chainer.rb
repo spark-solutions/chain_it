e0e02cae83af605efd6065bfb3282782601c90a2
btolarz <btolarz@gmail.com>
2018-03-28 17:53:40 +0200

    Init

---
+++ lib/chainer.rb
@@ -0,0 +1,14 @@
+require "chainer/version"
+
+class Chainer
+  def chain
+    return self if @skip
+    @result = yield @result&.value
+    @skip = true if @result.failure?
+    self
+  end
+
+  def result
+    @result
+  end
+end
---
+++ lib/chainer/version.rb
@@ -0,0 +1,3 @@
+module Chainer
+  VERSION = "0.1.0"
+end
